fc05ed6c13f07534d877c9161074fbb6a533018d
Luke Horvat <lukehorvat@gmail.com>
2017-10-24 09:18:40 +1100

    Implement basic mouseover magnification

--- src/scripts/dock.jsx
+++ src/scripts/dock.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 
 const defaultProps = {
-  width: "100%",
-  height: "100%"
+  backgroundColor: "#ccc",
+  width: "500px",
 };
 
-export default function(props) {
-  const { width, height } = Object.assign({}, defaultProps, props);
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div style={{ width, height }}>
-    </div>
-  );
+    this.state = {
+      index: null,
+    };
+  }
+
+  render() {
+    const { backgroundColor, width } = Object.assign({}, defaultProps, this.props);
+
+    return (
+      <div className="dock" style={{ backgroundColor, width, }}>
+        {
+          Array.from({ length: 5 }).map((_, index) => (
+            <div
+              key={index}
+              className="dock-app"
+              style={{ flex: this.state.index === index ? "1.5" : "1" }}
+              onMouseEnter={this.onMouseEnter.bind(this, index)}
+              onMouseLeave={this.onMouseLeave.bind(this, index)}
+            >
+              <svg viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="50" />
+              </svg>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
+  onMouseEnter(index, event) {
+    this.setState({ index });
+  }
+
+  onMouseLeave(index, event) {
+    console.log("onMouseLeave", index);
+  }
 }
